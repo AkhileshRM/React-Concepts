4960a19575b05f7a83e7a6454a403babd274eacf
AkhileshRM <akhilesh.madhyastha09@gmail.com>
2025-11-15 12:34:31 +0530

    Swiggy Dummy API

--- exploringWorld/src/components/ItemList.js
+++ exploringWorld/src/components/ItemList.js
@@ -15,7 +15,7 @@ const ItemList = ({ items, children }) => {
     <div>
       {items?.map((item) => (
         <div
-          key={item.card.info.id}
+          key={item?.card?.info?.id}
           className="p-2 m-2 border-b-2 border-gray-200 text-left"
         >
           <div className="flex gap-28">
--- exploringWorld/src/utils/constants.js
+++ exploringWorld/src/utils/constants.js
@@ -2,4 +2,4 @@ export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/"
 
 export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-leuttuce-3624ld.png"
 
-export const Menu_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0059026&lng=77.5468264&restaurantId="
+export const Menu_API = "https://namastedev.com/api/v1/listRestaurantMenu/"
